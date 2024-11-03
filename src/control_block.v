6b6ad5ed226134ef19da45cc44b37f63fa629492
Revenantx86 <ryalcin@uwaterloo.ca>
2024-11-02 20:44:45 -0400

    final test ?

--- src/control_block.v
+++ src/control_block.v
@@ -23,12 +23,14 @@ wire [3:0] opcode = ui_in [3:0];
 
 
 // Assign Output //
-assign uo_out[7:0] = stage;
+assign uo_out[7:0] = control_signals[7:0];
 //assign uo_out[7:3] = 0;
+
 
 assign uio_oe = 8'hff;    // Configure the bidirectional pins to be all outputs
 //assign uio_oe = 8'b00;
-assign uio_out = 8'hff;
+assign uio_out[7] = 1;
+assign uio_out[6:0] = control_signals[14:8];
 
 /* Supported Instructions' Opcodes */
 localparam OP_HLT = 4'h0;
@@ -58,22 +60,110 @@ localparam SIG_REGB_LOAD_N = 1;         // \L_B
 localparam SIG_OUT_LOAD_N = 0;          // \L_O
 
 /* Internal Regs */
-reg [7:0] stage;
-//reg [15:0] control_signals;
+reg [2:0] stage;
+reg [14:0] control_signals;
 
 /* Micro-Operation Stages */
-//localparam T0 = 0, T1 = 1, T2 = 2, T3 = 3, T4 = 4, T5 = 5, IDLE = 6; 
+localparam T0 = 0, T1 = 1, T2 = 2, T3 = 3, T4 = 4, T5 = 5, IDLE = 6; 
 
 /* Stage Transition Logic */
 always @(posedge clk) begin
-    //
-    if(rst_n == 0)begin
-        stage <= 0;
+    if (!rst_n) begin           // Check if reset is asserted, if yes, put into a holding stage
+      stage <= 6;
     end
-    else begin
-        stage <= stage +1; 
+ 	else begin                   // If reset is not asserted, do the stages sequentially
+      if (stage == 6) begin        
+          stage <= 0;
+        end 
+        else if (stage == T0 || stage == T1 || 
+                 stage == T2 || stage == T3 || 
+                 stage == T4 || stage == T5) begin
+            // Valid stages
+            stage <= stage + 1; // Increment to the next stage
+        end else begin
+            // If the stage is not valid, set it to 6
+            stage <= 6; // Set to stage 6 
+        end
     end
-
 end
+
+/* Micro-Operation Logic */
+always @(negedge clk) begin
+    control_signals <= 15'b000111111100011; // All signals are deasserted
+
+    case(stage)
+        T0: begin
+            control_signals[SIG_PC_EN] <= 1;
+            control_signals[SIG_MAR_ADDR_LOAD_N] <= 0;
+        end 
+        T1: begin
+            if (opcode != OP_HLT) begin
+                control_signals[SIG_PC_INC] <= 1;
+            end
+        end
+        T2: begin
+            control_signals[SIG_RAM_EN_N] <= 0;
+            control_signals[SIG_IR_LOAD_N] <= 0;
+        end
+        T3: begin
+            case (opcode)
+                OP_ADD, OP_SUB, OP_LDA, OP_STA: begin
+                    control_signals[SIG_IR_EN_N] <= 0;
+                    control_signals[SIG_MAR_ADDR_LOAD_N] <= 0;
+                end
+                OP_OUT: begin
+                    control_signals[SIG_REGA_EN] <= 1;
+                    control_signals[SIG_OUT_LOAD_N] <= 0;
+                end
+                OP_JMP: begin
+                    control_signals[SIG_IR_EN_N] <= 0;
+                    control_signals[SIG_PC_LOAD] <= 1;
+                end
+                default: begin
+                // Do nothing (leave control_signals unchanged)
+                end
+            endcase
+        end
+        T4: begin
+            case (opcode)
+                OP_ADD, OP_SUB: begin
+                    control_signals[SIG_RAM_EN_N] <= 0;
+                    control_signals[SIG_REGB_LOAD_N] <= 0;
+                end
+                OP_LDA: begin
+                    control_signals[SIG_RAM_EN_N] <= 0;
+                    control_signals[SIG_REGA_LOAD_N] <= 0;
+                end
+                OP_STA: begin
+                    control_signals[SIG_REGA_EN] <= 1;
+                    control_signals[SIG_MAR_MEM_LOAD_N] <= 0;
+                end
+                default: begin
+                // Do nothing (leave control_signals unchanged)
+                end
+            endcase
+        end
+        T5: begin
+            case (opcode)
+                OP_ADD: begin
+                    control_signals[SIG_REGB_EN] <= 1;
+                    control_signals[SIG_REGA_LOAD_N] <= 0;
+                end
+                OP_SUB: begin
+                    control_signals[SIG_ADDER_SUB] <= 1;
+                    control_signals[SIG_REGB_EN] <= 1;
+                    control_signals[SIG_REGA_LOAD_N] <= 0;
+                end
+                OP_STA: begin
+                    control_signals[SIG_RAM_LOAD_N] <= 0;
+                end
+                default: begin
+                // Do nothing (leave control_signals unchanged)
+                end
+            endcase
+        end
+    endcase
+end
+
 
 endmodule
